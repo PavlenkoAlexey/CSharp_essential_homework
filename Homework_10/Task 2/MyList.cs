91baf29f97ab248b800723a586dd5ee8bf5ebaf3
Alexey Pavlenko <pav.alyosha@gmail.com>
2020-11-14 21:39:45 +0300

    Task 2

---
+++ Homework_10/Task 2/MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    class MyList<T>
+    {
+        private const int defaultSize = 5; // размер при успользовании конструктора по умолчанию
+        private const double growthIndex = 2; // во сколько раз увеличится массив при переполнении 
+        private T[] array;
+        private int count; // кол-во элементов на данный момент, поле тут только затем что в задании было явно указано что свойство должно быть только для чтения
+        public int Count => count;  // свойство для получения кол-ва элементов на данный момент
+        public int Capacity { get; set; } // кол-во элементов которое может храниться в массиве до переполнения
+        public MyList() : this(defaultSize) { } //конструктор по умолчанию
+        public MyList(int initialCapacity) // конструктор с явно заданным кол-вом элементов до переполнения
+        {
+            count = 0;
+            Capacity = initialCapacity;
+            array = new T[initialCapacity];
+        }
+
+        public T this[int index]
+        {
+            get => array[index];
+        }
+        public void Add(T newElement)
+        {
+            if (count < Capacity)
+            {
+                array[count++] = newElement;
+            }
+            else
+            {
+                array = Resize(array);
+                array[count++] = newElement;
+                Capacity = array.Length;
+            }
+        }
+        private T[] Resize(T[] array)
+        {
+            int newCapacity = (int)(Capacity * growthIndex);
+            T[] newArray = new T[newCapacity];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            return array = newArray;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Homework_10/Task 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<int> intList = new MyList<int>(2);
+
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            intList.Add(5);
+
+            intList.Capacity = 6;
+            int testIndex1 = intList[3]; // получение элемента по указанному индексу
+
+            intList.Show();
+
+            Console.WriteLine("\n" + $"Элементов в массиве intList сейчас: {intList.Count}");
+            Console.WriteLine($"А всего может быть записано до следующего увеличения: {intList.Capacity}");
+
+            Console.WriteLine();
+
+            MyList<string> stringList = new MyList<string>();
+
+            stringList.Add("aaa");
+            stringList.Add("bbb");
+            stringList.Add("ccc");
+            stringList.Add("ddd");
+            stringList.Add("eee");
+            stringList.Add("fff");
+            stringList.Add("ggg");
+
+            stringList.Capacity = 8;
+            string testIndex2 = stringList[3];
+
+            stringList.Show();
+            Console.WriteLine("\n" + $"Элементов в массиве stringList сейчас: {stringList.Count}");
+            Console.WriteLine($"А всего может быть записано до следующего увеличения: {stringList.Capacity}");
+            Console.WriteLine("\n" + "Получение элементов по индексу");
+            Console.WriteLine($"testIndex1: {testIndex1}, testIndex2: {testIndex2}");
+
+            Console.ReadLine();
+        }
+    }
+}
