68eacb8d66fb1d34bd69b274f4f4ddc036468860
Alexey Pavlenko <pav.alyosha@gmail.com>
2020-11-15 15:13:54 +0300

    Task 3

---
+++ Homework_10/Task 3/MyDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private List<TKey> keys = new List<TKey>();
+        private List<TValue> values = new List<TValue>();
+
+        public int Count => values.Count; // получение общего кол-ва элементов
+        public TValue this[TKey index] // получение эл-та по указанному индексу
+        {
+            get
+            {
+                for (int i = 0; i < keys.Count; i++)
+                {
+                    if (keys[i].Equals(index))
+                        return values[i];
+                }
+                return default;
+            }
+        }
+
+        public void AddNewPair(TKey key, TValue value) // добавление новой пары элементов
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+    }
+}
---
+++ Homework_10/Task 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary1 = new MyDictionary<string, string>();
+            dictionary1.AddNewPair("ручка", "pen");
+            dictionary1.AddNewPair("яблоко", "apple");
+            dictionary1.AddNewPair("стол", "table");
+            dictionary1.AddNewPair("таблетка", "pill");
+
+            Console.WriteLine($"На данный момент в словаре: {dictionary1.Count} пар(а/ы) слово-перевод.");
+            Console.WriteLine($"ручка: {dictionary1["ручка"]}");
+            Console.WriteLine($"стол: {dictionary1["стол"]}");
+            Console.WriteLine($"а перевода слова машина нет: {dictionary1["машина"]}");
+
+            Console.WriteLine();
+
+            MyDictionary<int, double> dictionary2 = new MyDictionary<int, double>();
+            dictionary2.AddNewPair(1, 14.88);
+            dictionary2.AddNewPair(2, 359);
+            dictionary2.AddNewPair(3, 22.8);
+            dictionary2.AddNewPair(4, 13.37);
+
+            Console.WriteLine($"1: {dictionary2[1]}");
+            Console.WriteLine($"2: {dictionary2[2]}");
+            Console.WriteLine($"3: {dictionary2[3]}");
+            Console.WriteLine($"4: {dictionary2[4]}");
+
+            Console.WriteLine($"5 - отсутствует: {dictionary2[5]}");
+
+
+            Console.ReadLine(); //delay
+        }
+    }
+}
