b0fc2864cd3675e480beb4657909863da3cb8c18
Alexey Pavlenko <pav.alyosha@gmail.com>
2020-11-15 18:41:05 +0300

    Homework 9

---
+++ Homework_9/Task 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task_2
+{
+    public delegate double MyDelegate(double x, double y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate Add = (x, y) => x + y;
+            MyDelegate Sub = (x, y) => x - y;
+            MyDelegate Mul = (x, y) => x * y;
+            MyDelegate Div = (x, y) =>
+            {
+                if (y != 0)
+                    return x / y;
+                else
+                {
+                    Console.WriteLine("Нельзя делить на ноль!");
+                    return x / y;
+                }
+
+            };
+
+            Console.WriteLine("Введите первое число");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите знак операции");
+            char op = char.Parse(Console.ReadLine());
+
+            double result = 0;
+            switch (op)
+            {
+                case '+':
+                    result = Add(x, y);
+                    break;
+                case '-':
+                    result = Sub(x, y);
+                    break;
+                case '*':
+                    result = Mul(x, y);
+                    break;
+                case '/':
+                    result = Div(x, y);
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели неправильный знак операции");
+                    break;
+            }
+
+            Console.WriteLine($"Результат операции: {result}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework_9/Task 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate[] delegates = new MyDelegate[]
+            {
+                new MyDelegate(ReturnRandom),
+                new MyDelegate(ReturnRandom),
+                new MyDelegate(ReturnRandom),
+                new MyDelegate(ReturnRandom),
+                new MyDelegate(ReturnRandom),
+            };
+            
+            MyDelegate2 mydelegate = (delegates) =>
+            {
+                double sum = 0;
+                foreach (MyDelegate @delegate in delegates)
+                {
+                    sum += @delegate();
+                }
+                return sum / delegates.Length;
+            };
+
+            double arithmeticalMean = mydelegate(delegates);
+            Console.WriteLine(arithmeticalMean);
+
+            Console.ReadLine();
+        }
+
+        delegate int MyDelegate();
+        delegate double MyDelegate2(MyDelegate[] delegates);
+        
+        static int ReturnRandom()
+        {
+            Random rnd = new Random();
+            return rnd.Next(5, 10);
+        }
+        
+    }
+}
